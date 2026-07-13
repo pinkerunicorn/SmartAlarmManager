29e4808f877ea96063fb25544d1edc27c5b3ae0f
Florian Graßinger <grassinger@ubyte.gmbh>
2026-07-13 09:53:51 +0200

    Fix: Correct argument type for TargetID in VISU_PostNotification (int instead of string)

--- SmartAlarmManager/module.php
+++ SmartAlarmManager/module.php
@@ -339,7 +339,7 @@ class SmartAlarmManager extends IPSModule
             if ($visu > 0 && IPS_InstanceExists($visu)) {
                 $this->LogMessage("App/Visu: Sende Push & Notification", KL_NOTIFY);
                 if (function_exists('VISU_PostNotification')) {
-                    VISU_PostNotification($visu, "Alarm!", $message, "Warning", "");
+                    VISU_PostNotification($visu, "Alarm!", $message, "Warning", 0);
                 }
             }
         }
@@ -408,7 +408,7 @@ class SmartAlarmManager extends IPSModule
             if ($visu > 0 && IPS_InstanceExists($visu)) {
                 $this->LogMessage("App/Visu: Sende Push & Notification", KL_NOTIFY);
                 if (function_exists('VISU_PostNotification')) {
-                    VISU_PostNotification($visu, "VOLLALARM", $message, "Alert", "Alarm");
+                    VISU_PostNotification($visu, "VOLLALARM", $message, "Alert", 0);
                 }
             }
         }
@@ -432,7 +432,7 @@ class SmartAlarmManager extends IPSModule
             if ($visu > 0 && IPS_InstanceExists($visu)) {
                 $this->LogMessage("App/Visu: Sende Push & Notification", KL_NOTIFY);
                 if (function_exists('VISU_PostNotification')) {
-                    VISU_PostNotification($visu, "Info", $message, "Information", "");
+                    VISU_PostNotification($visu, "Info", $message, "Information", 0);
                 }
             }
         }
